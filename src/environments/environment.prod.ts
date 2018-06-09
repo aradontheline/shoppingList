1509bd8887aa1627a3666731bc498ec06831bff3
Arad <aradontheline@gmail.com>
2018-06-09 22:24:16 +0430

    new update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  PARSE_APP_ID: 'zi0basfAu5LzlQshG5aNhoUq49j4bGXYBlQ7ey0f',
+  PARSE_JS_KEY: 'S8C4E3L0rSnJIBKUyag34g14UbHNvmcch4flV5wb',
+  serverURL: 'https://parseapi.back4app.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   PARSE_APP_ID: 'zi0basfAu5LzlQshG5aNhoUq49j4bGXYBlQ7ey0f',
   PARSE_JS_KEY: 'S8C4E3L0rSnJIBKUyag34g14UbHNvmcch4flV5wb',
   serverURL: 'https://parseapi.back4app.com'
